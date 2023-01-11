acafe7903aefddd08bf97b74b320bc0179b1a127
Yure Ribeiro <yureribeirocontato@gmail.com>
2023-01-11 17:05:03 -0300

    add chart.js

--- chart.js
+++ chart.js
@@ -1,23 +1,43 @@
+
+
 const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-  type: 'bar',
+ new Chart(ctx, {
+  type: 'line',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 2,
-      backgroundColor: 'rgb(107, 201, 177)',
+      label: 'Dolar Comercial 1A 2022',
+      data: [5.674, 5.242, 5.016, 4.690, 5.134, 4.989, 5.268, 5.085, 5.148, 5.191, 5.397, 5.312],
+      borderWidth: 3,
+      borderColor: 'rgba(107, 201, 177, 0.85)',
+      backgroundColor: 'transparent',
+      tension: 0.1,
+      pointHoverBackgroundColor: 'white'
+    },
+    {
+      label: 'Bitcoin 1A 2022',
+      data: [42.694, 43.517, 39.433, 42.285, 31.004, 22.494, 19.302, 24.316, 21.639, 19.155, 17.547, 17.115],
+      borderWidth: 3,
+      borderColor: 'rgba(200, 340, 100, 0.85)',
+      backgroundColor: 'transparent',
+      tension: 0.1,
+      pointHoverBackgroundColor: 'white'
     }]
   },
   options: {
-    scales: {
-      y: {
-        beginAtZero: true
+    plugins: {
+      title: {
+        display: true,
+        text: 'RELATÓRIO ANUAL 2022',
+        color: 'white',
+        padding: {
+          top: 10,
+        },    
       }
-    }
-  }
+    }}
 });
 
-export default Chart
+
+
+export default ctx
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cotas from "./cotacoes.js"
 import ConvertValues from "./conversor.js"
 import coinRank from "./cripto.js"
 import { FormSubmit, formSubmit } from "./form.js"
+import ctx from "./chart.js"
+
 
 
 
@@ -12,5 +14,5 @@ menu()
 coinRank()
 formSubmit
 FormSubmit
-
+ctx
 
